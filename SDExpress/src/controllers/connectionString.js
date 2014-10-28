343817ab932405f59d334c869c9fec31abd8b86d
TedZhang <Ted.Zhanghl@ef.com>
2014-10-28 09:44:29 +0800

    123

--- SDExpress/src/controllers/connectionString.js
+++ SDExpress/src/controllers/connectionString.js
@@ -10,22 +10,25 @@ DELETE	/efconfig/ConnectionString/?namespace=XX&environment=XX
 */
 
 router.get('/', function(req, res) {
-	res.send("1234");
 	
-			//debugger;
+	
+			debugger;
 			
-	var conn_str="Data Source=cns-etdbdevvs1;Initial Catalog=Rio;Persist Security Info=True;User ID=RioUser;Password=riouserdev";
+	var conn_str="Driver={SQL Server Native Client 11.0};Server=cns-etdbdevvs1;UID=RioUser;PWD=riouserdev;Database={Rio}";
 	 
 	sql.open(conn_str, function (err, conn) {
         if (err) {
             console.log('发生错误');
         }
 		
-        sql.queryRaw(conn_str, "select top 1 1", function (err, results) {
-            
+        sql.queryRaw(conn_str, "select top 1 * from RIO.DBO.UserInfo", function (err, results) {
+			var sb=[];
+			for(var i = 0;i<results.rows.length;i++){
+				sb.push(i+":"+results.rows[i])
+			}
+			res.send(sb.join(''));
         })
     });
-	res.send("12345");
 });
 
 router.delete('/', function(req, res) {  
